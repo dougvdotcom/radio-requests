a4afd25f6aca9cd75d0086dfcdd93ddf4d4f6a51
Doug Vanderweide <dougvanderweide@gmail.com>
2016-01-31 10:59:00 -0500

    Create admin_list.php

---
+++ admin/admin_list.php
@@ -0,0 +1,54 @@
+<?
+session_start();
+if(!isset($_SESSION['login']) || $_SESSION['login'] == "") {
+	header('Location: index.php');
+}
+require_once("../conn.inc.php");
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<!--
+Simple PHP / MySQL Radio Request System
+Copyright 2006 Doug Vanderweide
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+-->
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>DJ List</title>
+</head>
+<body>
+<h1>Admin List</h1>
+<?
+	$sql = "SELECT * FROM adminlist ORDER BY admin_id DESC";
+	$rs = mysql_query($sql) or die('Cannot get admin records');
+	if(mysql_num_rows($rs) == 0) {
+		echo "<p>There are no admins.</p>\n";
+	}
+	else {
+		echo "<table>\n";
+		echo "\t<tr>\n";
+		echo "\t\t<th>Edit</th><th>Admin Username</th><th>Delete</th>\n";
+		echo "\t</tr>\n";
+		while($row = mysql_fetch_array($rs)) {
+			echo "\t<tr>\n";
+			echo "\t\t<td><a href=\"admin_edit.php?id=$row[admin_id]\">Edit</a></td><td>$row[admin_username]</td><td><a href=\"admin_delete.php?id=$row[admin_id]\">Delete</a></td>\n";
+			echo "\t</tr>\n";
+		}
+		echo "</table>\n";
+	}
+?>
+<p><a href="admin_add.php">Add Admin</a> | <a href="menu.php">Menu</a> </p>
+<p>&nbsp;</p>
+</body>
+</html>
